b7c19a647aef4916706e894cc2a608c63bd35a70
Anirban <anirban21das@gmail.com>
2023-11-28 14:44:17 +0530

    custom playlust done for private

---
+++ MovieReviewAPI/Migrations/20231128082605_playlistAdd.cs
@@ -0,0 +1,345 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieReviewAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class playlistAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayListModelId",
+                table: "Movies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PlayLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayLists", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 28,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 35,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 36,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 37,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 38,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 39,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 40,
+                column: "PlayListModelId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_PlayListModelId",
+                table: "Movies",
+                column: "PlayListModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_PlayLists_PlayListModelId",
+                table: "Movies",
+                column: "PlayListModelId",
+                principalTable: "PlayLists",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_PlayLists_PlayListModelId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "PlayLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_PlayListModelId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "PlayListModelId",
+                table: "Movies");
+        }
+    }
+}
---
+++ MovieReviewAPI/Models/PlayList/PlayListModel.cs
@@ -0,0 +1,17 @@
+﻿using MovieReviewAPI.Models.MovieList;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieReviewAPI.Models.PlayList
+{
+    public class PlayListModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public List<MovieListModel> Movies { get; set; } = new List<MovieListModel>();
+    }
+}
